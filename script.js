75b8c3d6565c6dffb8b9a6ac084c579a35aaceb0
Try <127165831+tryzwk@users.noreply.github.com>
2023-11-01 13:32:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // Animação de Fade In
+    $("#fadeInButton").click(function() {
+        $("#inicio").fadeIn();
+    });
+});
+
+function exibirMensagem() {
+    alert("Abrir Whatsapp?, Clique em OK")
+    
+}
